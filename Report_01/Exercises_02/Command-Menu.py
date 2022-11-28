e2ecbc1d1918053a48fcf44b6387b9669e81261f
helhadad <helhadad@gmail.com>
2022-11-28 23:29:23 +0000

    Add feature to Report_01/Exercise_2: Add two menu items [7] and [8]

--- Report_01/Exercises_02/Command-Menu.py
+++ Report_01/Exercises_02/Command-Menu.py
@@ -13,33 +13,42 @@ print("------------------------------------------------------------")
 print("[1] - Command 1               [4] - Command 4 ")
 print("[2] - Command 2               [5] - Command 5 ")
 print("[3] - Command 3               [6] - Command 6 ")
+print("[7] - Command 7               [8] - Command 8 ")
 print("[0] - Exit                                                   ")
 
 choice = input("Select command by entering asociated number (i.e. 1 or 2 or 4 etc...) : ")
 print("")
 while choice != "0":
     if choice == "1":
-        print("You have slected command 1")
+        print("You have selected command 1")
         # Do something here
         break
     elif choice == "2":
-        print("You have slected command 2")
+        print("You have selected command 2")
         # Do something here
         break
     elif choice == "3":
-        print("You have slected command 3")
+        print("You have selected command 3")
         # Do something here
         break
     elif choice == "4":
-        print("You have slected command 4")
+        print("You have selected command 4")
         # Do something here
         break
     elif choice == "5":
-        print("You have slected command 5")
+        print("You have selected command 5")
         # Do something here
         break
     elif choice == "6":
-        print("You have slected command 6")
+        print("You have selected command 6")
+        # Do something here
+        break
+    elif choice == "7":
+        print("You have selected command 7")
+        # Do something here
+        break
+    elif choice == "8":
+        print("You have selected command 8")
         # Do something here
         break
 else:
