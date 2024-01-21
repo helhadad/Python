19b9dac7ebc2a11d30653a0f279d458535f08be6
helhadad <helhadad@gmail.com>
2024-01-21 19:00:59 +0000

    Adding space before port number

--- Report_01/Exercises_04/Flow_Control.py
+++ Report_01/Exercises_04/Flow_Control.py
@@ -61,7 +61,7 @@ for x in fruits:
 
 scan = {"192.168.3.10": "80", "192.168.3.11": "443", "192.168.3.55": "22"}
 for ipv4, port in scan.items():
-    print(f"Found a service on{ipv4} at {port}")
+    print(f"Found a service on {ipv4} at {port}")
 
 # -------------------------------- L I S T  C O M P R E H E N S I I O N --------------------------
 # Answer to exercise
